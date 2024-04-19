50e85ee1bf6b925e46e947881bf477123c051e76
bluejynz <dany_falk@hotmai.com>
2024-04-19 19:15:10 -0300

    temporary description on homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
 function Home() {
     return (
         <>
-            <h1>Dungeon Starter</h1>
+            <h1 className="text-xl">Dungeon Starter</h1>
+            <p>Este app está em desenvolvimento.</p>
+            <p>
+                Clique ali em cima em "Create character" para testar a criação
+                de personagem que temos até agora.
+            </p>
         </>
     );
 }
